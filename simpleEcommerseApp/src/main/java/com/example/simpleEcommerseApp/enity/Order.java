49759066e826110b23e93102cbedd912880aa38a
Pralhad Bhusari <bhusaripralhad11@gmail.com>
2026-07-20 19:12:32 +0530

    lombok add

--- simpleEcommerseApp/src/main/java/com/example/simpleEcommerseApp/enity/Order.java
+++ simpleEcommerseApp/src/main/java/com/example/simpleEcommerseApp/enity/Order.java
@@ -4,15 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@getter
+@Table(name="orders")
+@Getter
 @Setter
-
-
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
