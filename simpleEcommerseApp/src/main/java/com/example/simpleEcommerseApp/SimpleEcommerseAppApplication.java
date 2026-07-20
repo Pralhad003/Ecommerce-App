a973e76d3fda0c6acb93ee603c5ca84b325bcf91
Pralhad Bhusari <bhusaripralhad11@gmail.com>
2026-07-20 18:25:48 +0530

    Initial Spring Boot project

---
+++ simpleEcommerseApp/src/main/java/com/example/simpleEcommerseApp/SimpleEcommerseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleEcommerseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcommerseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcommerseAppApplication.class, args);
+	}
+
+}
---
+++ simpleEcommerseApp/src/test/java/com/example/simpleEcommerseApp/SimpleEcommerseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleEcommerseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
